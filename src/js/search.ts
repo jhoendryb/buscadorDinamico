025b9439af9c4977d10b4ab49c5ce3ce58e81ffa
JhoendryB <jhoendryblanco@htmail.com>
2026-06-16 01:47:08 -0400

    xample html

--- src/js/search.ts
+++ src/js/search.ts
@@ -31,119 +31,119 @@ const search1 = new Search({
 });
 
 
-// const prueba = search1.on('renderItems', (data) => {
-//     const { content } = data;
-//     const item = content.children;
-//     console.log('Page change event:', item[0]);
-//     console.log('Item content:', item[0].innerHTML);
-// });
+const prueba = search1.on('renderItems', (data) => {
+    const { content } = data;
+    const item = content.children;
+    console.log('Page change event:', item[0]);
+    console.log('Item content:', item[0].innerHTML);
+});
 
-// console.log("Esto es pruebas", prueba);
+console.log("Esto es pruebas", prueba);
 
-// search1.on('itemSelected', (data) => {
-//     console.log('Item seleccionado:', data.item);
-// });
+search1.on('itemSelected', (data) => {
+    console.log('Item seleccionado:', data.item);
+});
 
-// const search2 = new Search({
-//     element: '.app-search2'
-// });
+const search2 = new Search({
+    element: '.app-search2'
+});
 
-// search2.on('search', (data: any) => {
-//     console.log('Search event:', data);
-// });
+search2.on('search', (data: any) => {
+    console.log('Search event:', data);
+});
 
-// search2.on('pageChange', (data: any) => {
-//     console.log('Page change event:', data);
-// });
+search2.on('pageChange', (data: any) => {
+    console.log('Page change event:', data);
+});
 
-// const search3 = new Search({
-//     element: '.app-search3'
-// });
+const search3 = new Search({
+    element: '.app-search3'
+});
 
-// const search4 = new Search({
-//     element: '.app-search4',
-//     data: [
-//         {
-//             country: 'VE',
-//             name: 'Venezuela',
-//             descripcion: 'El pais mas rico en petroleo.'
-//         },
-//         {
-//             country: 'CO',
-//             name: 'Colombia',
-//             descripcion: 'El pais mas rico en cafe.'
-//         },
-//         {
-//             country: 'MX',
-//             name: 'Mexico',
-//             descripcion: 'El pais mas rico en tacos.'
-//         },
-//         {
-//             country: 'AR',
-//             name: 'Argentina',
-//             descripcion: 'El pais mas rico en empanadas.'
-//         },
-//         {
-//             country: 'CL',
-//             name: 'Chile',
-//             descripcion: 'El pais mas rico en vino.'
-//         },
-//         {
-//             country: 'PE',
-//             name: 'Peru',
-//             descripcion: 'El pais mas rico en machi.'
-//         },
-//         {
-//             country: 'EC',
-//             name: 'Ecuador',
-//             descripcion: 'El pais mas rico en cacao.'
-//         },
-//         {
-//             country: 'BO',
-//             name: 'Bolivia',
-//             descripcion: 'El pais mas rico en plata.'
-//         },
-//         {
-//             country: 'CO',
-//             name: 'Colombia',
-//             descripcion: 'El pais mas rico en cafe.'
-//         },
-//         {
-//             country: 'MX',
-//             name: 'Mexico',
-//             descripcion: 'El pais mas rico en tacos.'
-//         },
-//         {
-//             country: 'AR',
-//             name: 'Argentina',
-//             descripcion: 'El pais mas rico en empanadas.'
-//         },
-//         {
-//             country: 'CL',
-//             name: 'Chile',
-//             descripcion: 'El pais mas rico en vino.'
-//         },
-//         {
-//             country: 'PE',
-//             name: 'Peru',
-//             descripcion: 'El pais mas rico en machi.'
-//         },
-//         {
-//             country: 'EC',
-//             name: 'Ecuador',
-//             descripcion: 'El pais mas rico en cacao.'
-//         },
-//         {
-//             country: 'BO',
-//             name: 'Bolivia',
-//             descripcion: 'El pais mas rico en plata.'
-//         },
-//     ]
-// });
+const search4 = new Search({
+    element: '.app-search4',
+    data: [
+        {
+            country: 'VE',
+            name: 'Venezuela',
+            descripcion: 'El pais mas rico en petroleo.'
+        },
+        {
+            country: 'CO',
+            name: 'Colombia',
+            descripcion: 'El pais mas rico en cafe.'
+        },
+        {
+            country: 'MX',
+            name: 'Mexico',
+            descripcion: 'El pais mas rico en tacos.'
+        },
+        {
+            country: 'AR',
+            name: 'Argentina',
+            descripcion: 'El pais mas rico en empanadas.'
+        },
+        {
+            country: 'CL',
+            name: 'Chile',
+            descripcion: 'El pais mas rico en vino.'
+        },
+        {
+            country: 'PE',
+            name: 'Peru',
+            descripcion: 'El pais mas rico en machi.'
+        },
+        {
+            country: 'EC',
+            name: 'Ecuador',
+            descripcion: 'El pais mas rico en cacao.'
+        },
+        {
+            country: 'BO',
+            name: 'Bolivia',
+            descripcion: 'El pais mas rico en plata.'
+        },
+        {
+            country: 'CO',
+            name: 'Colombia',
+            descripcion: 'El pais mas rico en cafe.'
+        },
+        {
+            country: 'MX',
+            name: 'Mexico',
+            descripcion: 'El pais mas rico en tacos.'
+        },
+        {
+            country: 'AR',
+            name: 'Argentina',
+            descripcion: 'El pais mas rico en empanadas.'
+        },
+        {
+            country: 'CL',
+            name: 'Chile',
+            descripcion: 'El pais mas rico en vino.'
+        },
+        {
+            country: 'PE',
+            name: 'Peru',
+            descripcion: 'El pais mas rico en machi.'
+        },
+        {
+            country: 'EC',
+            name: 'Ecuador',
+            descripcion: 'El pais mas rico en cacao.'
+        },
+        {
+            country: 'BO',
+            name: 'Bolivia',
+            descripcion: 'El pais mas rico en plata.'
+        },
+    ]
+});
 
 search1.init();
-// search2.init();
-// search3.init();
-// search4.init();
+search2.init();
+search3.init();
+search4.init();
 
-// search2.sort('name', 'asc').draw();
+search2.sort('name', 'asc').draw();
